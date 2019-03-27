2c8b914ec4355bf6a19d70a3997ce147090d7dfd
Evan Schwartz <evan.mark.schwartz@gmail.com>
2019-03-27 19:59:50 -0400

    test(redis): make sure reuse address is set

--- crates/interledger/tests/redis.rs
+++ crates/interledger/tests/redis.rs
@@ -23,7 +23,7 @@ pub struct RedisServer {
 fn get_open_port(try_port: Option<u16>) -> u16 {
     if let Some(port) = try_port {
         let listener = net2::TcpBuilder::new_v4().unwrap();
-        listener.reuse_address(true).unwrap();
+        let listener = listener.reuse_address(true).unwrap();
         if let Ok(listener) = listener.bind(&format!("127.0.0.1:{}", port)) {
             return listener.listen(1).unwrap().local_addr().unwrap().port();
         }
@@ -31,7 +31,7 @@ fn get_open_port(try_port: Option<u16>) -> u16 {
 
     for _i in 0..1000 {
         let listener = net2::TcpBuilder::new_v4().unwrap();
-        listener.reuse_address(true).unwrap();
+        let listener = listener.reuse_address(true).unwrap();
         if let Ok(listener) = listener.bind("127.0.0.1:0") {
             return listener.listen(1).unwrap().local_addr().unwrap().port();
         }
