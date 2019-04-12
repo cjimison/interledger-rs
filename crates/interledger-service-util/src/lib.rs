2db9028a66a833e5f7e93ff1e76b198f4618086e
Evan Schwartz <evan.mark.schwartz@gmail.com>
2019-04-12 19:06:16 -0400

    feat: add rate limiting
    provides both packet-based limits and money throughput limits
    uses redis-cell module for the redis implementation

--- crates/interledger-service-util/src/lib.rs
+++ crates/interledger-service-util/src/lib.rs
@@ -7,6 +7,7 @@ extern crate log;
 
 mod expiry_shortener;
 mod max_packet_amount;
+mod rate_limit;
 mod rates_and_balances;
 mod validator;
 
@@ -14,6 +15,7 @@ pub use self::expiry_shortener::{
     ExpiryShortenerService, RoundTripTimeAccount, DEFAULT_ROUND_TRIP_TIME,
 };
 pub use self::max_packet_amount::{MaxPacketAmountAccount, MaxPacketAmountService};
+pub use self::rate_limit::{RateLimitAccount, RateLimitError, RateLimitService, RateLimitStore};
 pub use self::rates_and_balances::{
     BalanceStore, ExchangeRateAndBalanceService, ExchangeRateStore,
 };
